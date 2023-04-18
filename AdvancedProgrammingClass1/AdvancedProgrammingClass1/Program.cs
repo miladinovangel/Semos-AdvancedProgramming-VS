ade9731f2c7c0df4054bb98bd5bb4903c9a8180c
miladinovangel <miladinovangel95@gmail.com>
2023-04-18 20:51:03 +0200

    zadaca 1 so resenie + zadaca 2

---
+++ AdvancedProgrammingClass1/AdvancedProgrammingClass1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Console.WriteLine("Hello, World!");
+// 1. Promenlivi/Variables
+//int
+//Console.WriteLine(5);
+// float
+//Console.WriteLine(3.18);
+//string
+int angel = 5;
+Console.WriteLine(angel);
